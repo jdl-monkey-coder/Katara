d30b3bb2aa837fc71c2a13e467f02be3aa4e6457
JDLEE <josephdlee08@gmail.com>
2021-01-02 12:14:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,22 @@ async def ping(ctx):
 @client.command()
 async def hiya(ctx):
     await ctx.send('hey')
+    
+@client.command()
+async def count(ctx):
+    for i in range(1, 11):
+        await ctx.send(i, ' potato')
+
+@client.command()
+async def countdown(ctx):
+    await ctx.send('3')
+    await asyncio.sleep(1)
+    await ctx.send('2')
+    await asyncio.sleep(1)
+    await ctx.send('1')
+    await asyncio.sleep(1)
+    holidays = ['Happy Thanksgiving!', 'Happy Hanukkah!', 'Merry Christmas!', 'Happy New Year!']
+    for holiday in holidays:
+        await ctx.send(random.choice(holidays))
 
 client.run('Nzg5MTUxOTc5MjgwMTM4MjYw.X9t5DQ.L7c3iLvDNdIfp-T35G6kF8nNtVE')
