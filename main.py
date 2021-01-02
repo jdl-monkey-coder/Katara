ea0623a5f4032f40486c71a8544ee529dc0deea3
JDLEE <josephdlee08@gmail.com>
2021-01-02 11:47:07 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio 
+
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.ext import tasks
+
+
+
+
+client = commands.Bot(command_prefix = 'm.', case_insensitive=True)
+
+#a few simple commands and events
+
+@client.event
+async def on_ready():
+    print('I AM READY!')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server!')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server...')
+
+@client.command(alias=['yeet'])
+async def ping(ctx):
+    await ctx.send(f'My ping is {client.latency}!')
+
+client.run('Nzg5MTUxOTc5MjgwMTM4MjYw.X9t5DQ.L7c3iLvDNdIfp-T35G6kF8nNtVE')
